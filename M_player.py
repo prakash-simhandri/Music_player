131cdb0ed29d42a08fe65466ac8656bb90383c13
prakash simhandri <44014887+prakash-simhandri@users.noreply.github.com>
2019-11-16 21:48:05 +0530

    Update M_player.py

--- M_player.py
+++ M_player.py
@@ -1,25 +1,9 @@
-# import pygame  
-# import time
-# file = '/home/pandu/Desktop/my_data/AI/spwch_work/I Got You.mp3'
-# pygame.init()
-# pygame.mixer.init()
-# pygame.mixer.music.load(file)
-# pygame.mixer.music.play()
-# time.sleep(20)
-# pygame.mixer.music.pause()
-# time.sleep(5)
-# pygame.mixer.music.unpause()
-# pygame.event.wait()
-
-
-# ============================
-
-import pygame # sudo apt install libsdl2-dev libfreetype6-dev libsdl2-mixer-dev libsdl2-image-dev libsdl2-ttf-dev libjpeg-dev libpng-dev libportmidi-dev
+import pygame
 import random, os
 from os import path
 import json,time
 
-music_data = '/home/pandu/Music/Englesh_songs' # enter your music directory
+music_data = '/home/-/-/' # enter your music directory
 songs_list = os.listdir(music_data)
 x= 0
 if path.exists("Emoji.json"):
@@ -35,7 +19,7 @@ try:
 		    pygame.init()
 		    pygame.mixer.init()
 		    clock = pygame.time.Clock()
-		    pygame.mixer.music.load("/home/pandu/Music/Englesh_songs/"+songs_list[x]) # enter your music directory
+		    pygame.mixer.music.load("/home/-/-/"+songs_list[x]) # enter your music directory
 		    pygame.mixer.music.play(0)
 
 		    # Wait for the music to play before exiting 
@@ -43,7 +27,7 @@ try:
 		    	emoji=random.choice(emoji_list)
 		    	time.sleep(2)
 		        print(emoji.decode('unicode-escape'))
-		        clock.tick(1)  #This is class that will help us to track amount of time or to manage framerate.
+		        clock.tick(1)  	#This is class that will help us to track amount of time or to manage framerate.
 		    que()
 		except Exception as e:
 			user_choice=raw_input({e:"Do you want listening again: YES/NO >>"})
@@ -63,7 +47,7 @@ try:
 	    if int(pos) == -1:
 	        x += 1
 	        clock = pygame.time.Clock()
-	        pygame.mixer.music.load("/home/pandu/Music/Englesh_songs/"+songs_list[x]) # enter your music directory
+	        pygame.mixer.music.load("/home/-/-/"+songs_list[x]) 	# enter your music directory
 	        pygame.mixer.music.play(0)
 	        
 	        # Wait for the music to play before exiting 
